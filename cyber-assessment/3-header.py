f692ead0e907b8088979a5913fabd48fde7b61af
Your Name <you@example.com>
2026-03-22 20:29:53 +0000

    correct ex3

--- cyber-assessment/3-header.py
+++ cyber-assessment/3-header.py
@@ -4,6 +4,6 @@ import urllib.request
 
 try:
     with urllib.request.urlopen(sys.argv[1]) as r:
-        print(r.headers.get('X'))
+        print(r.headers.get('X-Request-Id'))
 except Exception:
     print("Request failed")
